93314200cd0ee8ef5d04579316926bcf5c7e5e9d
xuzhiweilgd <1451535983@qq.com>
2016-10-10 10:50:05 +0800

    测试

---
+++ testAjax.php
@@ -0,0 +1,77 @@
+{
+"records":
+[
+{
+"Name" : "许职位",
+"itemId":"1",
+"City" : "北京",
+"Country" : "Germany"
+},
+{
+"Name" : "王凡",
+"itemId":"2",
+"City" : "上海",
+"Country" : "Sweden"
+},
+{
+"Name" : "夏克",
+"itemId":"3",
+"City" : "重庆",
+"Country" : "Mexico"
+},
+{
+"Name" : "王德宝",
+"itemId":"4",
+"City" : "山东",
+"Country" : "Austria"
+},
+{
+"Name" : "王凡",
+"itemId":"2",
+"City" : "上海",
+"Country" : "Sweden"
+},
+{
+"Name" : "夏克",
+"itemId":"3",
+"City" : "重庆",
+"Country" : "Mexico"
+},
+{
+"Name" : "王德宝",
+"itemId":"4",
+"City" : "山东",
+"Country" : "Austria"
+},
+{
+"Name" : "王德宝",
+"itemId":"4",
+"City" : "山东",
+"Country" : "Austria"
+},
+{
+"Name" : "王德宝",
+"itemId":"4",
+"City" : "山东",
+"Country" : "Austria"
+},
+{
+"Name" : "王德宝",
+"itemId":"4",
+"City" : "山东",
+"Country" : "Austria"
+},
+{
+"Name" : "王德宝",
+"itemId":"4",
+"City" : "山东",
+"Country" : "Austria"
+},
+{
+"Name" : "王德宝",
+"itemId":"4",
+"City" : "山东",
+"Country" : "Austria"
+}
+]
+}
